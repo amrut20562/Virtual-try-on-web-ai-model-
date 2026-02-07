01e0740d634d5793edf2e7012cd0ad975cdfb1fe
Amrut More <amrut20562@gmail.com>
2026-02-07 11:26:16 +0530

    token related changes

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 DEVICE = "cuda"
 
-HF_TOKEN = os.getenv("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")  # <-- ADD THIS
+HF_TOKEN = os.getenv("hf_token")  # <-- ADD THIS
 
 SD_INPAINT_MODEL = "runwayml/stable-diffusion-inpainting"
 CONTROLNET_MODEL = "lllyasviel/control_v11p_sd15_openpose"
@@ -12,3 +12,4 @@ IP_ADAPTER_MODEL = "h94/IP-Adapter"
 IP_ADAPTER_ENCODER = "laion/CLIP-ViT-H-14-laion2B-s32B-b79K"
 
 IMAGE_SIZE = 512
+
